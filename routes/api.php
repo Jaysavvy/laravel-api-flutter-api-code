cb491969fdf92762f649c6baa7a7e7fcbc917aec
Jay Lovinsky <jcheckside@gmail.com>
2025-05-23 16:18:51 -0400

    API endpoint with a Middleware

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\TransactionController;
 
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// ...
 
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('transactions', TransactionController::class);
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('transactions', TransactionController::class);
+});
